be09a5ae4b3a6d778c104556a8591f03a0b54e4d
Brandon Rukin <rukin.brandon@gmail.com>
2015-12-16 23:40:45 -0500

    #3 - Using PostCSS with Sass in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,19 @@
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 var csswring = require('csswring');
+var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
 	var processors = [
 		csswring
 	];
 
-	return gulp.src('styles.css')
+	return gulp.src('styles.scss')
+		.pipe(sass())
 		.pipe(postcss(processors))
 		.pipe(gulp.dest('./dest'));
 });
 
 gulp.task('watch:styles', function() {
-	gulp.watch('**/*.css', ['styles']);
+	gulp.watch('**/*.scss', ['styles']);
 });
