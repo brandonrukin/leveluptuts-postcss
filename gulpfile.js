7ec631c36060a13aa5e2484cffe069df0ccd642f
Brandon Rukin <rukin.brandon@gmail.com>
2015-12-17 19:40:49 -0500

    #15 - Awesome Responsive Text with Rucksack

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer-core');
 var lost = require('lost');
+var rucksack = require('rucksack-css');
 
 gulp.task('styles', function() {
 	var processors = [
 		lost,
+		rucksack,
 		autoprefixer({browsers:['last 2 version']})
 	];
 
